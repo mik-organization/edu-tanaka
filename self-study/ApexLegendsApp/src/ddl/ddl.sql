c98118aad152217101578fe0394fa6b979960302
mik-tanaka <a-tanaka@mik-s.jp>
2025-02-14 11:52:00 +0900

    テーブル・列に論理名追加

--- self-study/ApexLegendsApp/src/ddl/ddl.sql
+++ self-study/ApexLegendsApp/src/ddl/ddl.sql
@@ -1,34 +1,34 @@
 /* Apexマスタ作成 */
 -- apex_db.m_apex definition
 create table m_apex (
-  regends_id int not null auto_increment,
-  regends_name varchar(32) not null,
-  real_name varchar(32) not null,
-  age varchar(32) not null,
-  gender varchar(32) not null,
-  regends_class varchar(32) not null,
-  abilities varchar(32) not null,
-  abi_description varchar(255) not null,
-  passive varchar(32) not null,
-  pas_description varchar(255) not null,
-  ult varchar(32) not null,
-  ult_description varchar(255) not null,
-  words varchar(255) not null,
-  picture_path varchar(255) not null,
+  regends_id int not null auto_increment comment 'レジェンドID',
+  regends_name varchar(32) not null comment 'レジェンド名',
+  real_name varchar(32) not null comment '本名',
+  age varchar(32) not null comment '年齢',
+  gender varchar(32) not null comment '性別',
+  regends_class varchar(32) not null comment 'クラス名',
+  abilities varchar(32) not null comment 'アビリティ名',
+  abi_description varchar(255) not null comment 'アビリティの説明',
+  passive varchar(32) not null comment 'パッシブ名',
+  pas_description varchar(255) not null comment 'パッシブの説明',
+  ult varchar(32) not null comment 'アルティメットアビリティ名',
+  ult_description varchar(255) not null comment 'アルティメットアビリティの説明',
+  words varchar(255) not null comment 'セリフ',
+  picture_path varchar(255) not null comment '画像パス',
   primary key (regends_id)
-) engine = innodb default charset = utf8mb4;
+) engine = innodb default charset = utf8mb4 comment='レジェンドマスタテーブル';
 
 /* レビューテーブル作成 */
 -- apex_db.t_review definition
 create table t_review (
-  review_id int not null auto_increment,
-  regends_id int not null,
-  user_name varchar(32) not null,
-  play_date date not null,
-  rating int not null,
-  age int not null,
-  comment varchar(255) not null,
+  review_id int not null auto_increment comment 'レビューID',
+  regends_id int not null comment 'レジェンドID',
+  user_name varchar(32) not null comment '利用者名',
+  play_date date not null comment 'プレイ日',
+  rating int not null comment '評価',
+  age int not null comment '年齢',
+  comment varchar(255) not null comment 'コメント',
   primary key (review_id),
   key regends_id (regends_id),
   constraint t_review_ibfk_1 foreign key (regends_id) references m_apex (regends_id)
-) engine = innodb default charset = utf8mb4;
+) engine = innodb default charset = utf8mb4 comment='レビュー情報を格納するテーブル';
