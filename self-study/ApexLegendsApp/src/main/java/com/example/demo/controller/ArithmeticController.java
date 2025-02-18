3561122f591a1b081376dca9e4a1ae8229d1264b
mik-tanaka <a-tanaka@mik-s.jp>
2025-02-18 17:16:07 +0900

    ArithmeticController.java作成

---
+++ self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/ArithmeticController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@GetMapping("/")
+public class ArithmeticController {
+	
+  /**
+   * GETメソッド
+   * 
+   */
+	
+}
