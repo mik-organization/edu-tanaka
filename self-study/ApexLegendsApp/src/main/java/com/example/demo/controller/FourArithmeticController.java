ee79ad00e4b988ff511061c55ae8926cf667180b
mik-tanaka <a-tanaka@mik-s.jp>
2025-03-03 11:07:04 +0900

    不要処理削除validationRequestメソッド

--- self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/FourArithmeticController.java
+++ self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/FourArithmeticController.java
@@ -25,9 +25,6 @@ public class FourArithmeticController {
   @PostMapping("/add")
   public int add(@Valid @RequestBody NumJsonRequest request, BindingResult result) throws BindException {
 
-    // 入力内容チェックし、エラーがある場合はメッセージを返す
-    Validation.validationRequest(result);
-
     // 正常な場合は計算結果を返す
     return request.getNum1() + request.getNum2();
   }
@@ -38,9 +35,6 @@ public class FourArithmeticController {
   @PostMapping("/subtract")
   public int subtract(@Valid @RequestBody NumJsonRequest request, BindingResult result) throws BindException {
 
-    // 入力内容チェックし、エラーがある場合はメッセージを返す
-    Validation.validationRequest(result);
-
     // 正常な場合は計算結果を返す
     return request.getNum1() - request.getNum2();
   }
@@ -50,9 +44,6 @@ public class FourArithmeticController {
    */
   @PostMapping("/multiply")
   public BigDecimal multiply(@Valid @RequestBody NumJsonRequest request, BindingResult result) throws BindException {
-
-    // 入力内容チェックし、エラーがある場合はメッセージを返す
-    Validation.validationRequest(result);
 
     // 正常な場合は、BigDecimal型に変換し計算結果を返す
     BigDecimal bigDecimalNum1 = BigDecimal.valueOf(request.getNum1());
@@ -68,8 +59,6 @@ public class FourArithmeticController {
   @PostMapping("/divide")
   public int divide(@Valid @RequestBody NumJsonRequest request, BindingResult result) throws BindException {
 
-    // 入力内容チェックし、エラーがある場合はメッセージを返す
-    Validation.validationRequest(result);
     Validation.validationDivideRequest(request); // 除数0エラー
 
     // 正常な場合は計算結果を返す
--- self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/Validation.java
+++ self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/Validation.java
@@ -1,19 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-
 import com.example.demo.controller.model.NumJsonRequest;
 
 public class Validation {
-
-  public static void validationRequest(BindingResult result) throws BindException {
-
-    // 入力内容にエラーがある場合は、エラーメッセージを返す
-    if (result.hasErrors()) {
-      throw new BindException(result);
-    }
-  }
 
   // 除数が0の場合、エラー表示
   public static void validationDivideRequest(NumJsonRequest request) throws IllegalArgumentException {
