4e68a02d1212a847439dabee365ccedc6387812f
mik-tanaka <a-tanaka@mik-s.jp>
2025-03-07 09:32:14 +0900

    Update self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/validator/Validation.java
    Co-authored-by: mik-yoshioka <67355045+mikyoshioka@users.noreply.github.com>

--- self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/validator/Validation.java
+++ self-study/ApexLegendsApp/src/main/java/com/example/demo/controller/validator/Validation.java
@@ -4,7 +4,6 @@ import com.example.demo.model.request.CalcRequest;
 
 public class Validation {
 
-  // 除数が0の場合、エラー表示
   public static void validationDivideRequest(CalcRequest request) throws IllegalArgumentException {
 
     if (request.getNum2() == 0) {
